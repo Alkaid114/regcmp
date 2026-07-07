701fedebba1a725fd60325ac1f6a3913e6440d9a
Alkaid <alkaid366@gmail.com>
2026-07-07 16:50:21 +0800

    feat: 添加WASM构建和GitHub Pages部署

---
+++ www/src/lib.rs
@@ -0,0 +1,10 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn compare(regex1: &str, regex2: &str) -> String {
+    match regcmp::compare(regex1, regex2) {
+        Ok(true) => "等价".to_string(),
+        Ok(false) => "不等价".to_string(),
+        Err(e) => format!("错误: {}", e),
+    }
+}
